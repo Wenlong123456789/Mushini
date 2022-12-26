11188a15f368d2b425d101028b96e96f41c0c2b0
Wenlong123456789 <92168128+Wenlong123456789@users.noreply.github.com>
2022-12-26 23:47:23 +0800

    Update Mushini.js

--- Mushini.js
+++ Mushini.js
@@ -1,7 +1,6 @@
 # Mushini
-私人订阅
 /*******************************
-  公众号:Mushini
+  公众号:木木IOS分享
 关注了解更多新科技！！！
 巧影破解订阅
 脚本名称:巧影破解订阅
@@ -10,19 +9,19 @@
 群1077223830
 *******************************
 [rewrite_local]
-^http[s]?:\/\/api-account.kinemasters.com\/v2\/user\/subscribe.+$ url script-response-body Mushini.js
+^http[s]?:\/\/api-account.kinemasters.com\/v2\/user\/subscribe.+$ url script-response-body kinemasters.js
 [mitm] 
 hostname = *.kinemasters.*
 *******************************
 Surge
 
 [Script]
-^http[s]?:\/\/api-account.kinemasters.com\/v2\/user\/subscribe.+$ requires-body=1,max-size=0,script-path=Mushini.js
+^http[s]?:\/\/api-account.kinemasters.com\/v2\/user\/subscribe.+$ requires-body=1,max-size=0,script-path=kinemasters.js
 
 [MITM]
 hostname = *.kinemasters.*
 
 *******************************/
 var obj = JSON.parse($response.body);
-    obj.data.isSubscribed= true;
+    obj.isSubscribed= true;
     $done({body: JSON.stringify(obj)});
