501a86ee28c28ac75945a149a67f1645f9405a12
Wenlong123456789 <92168128+Wenlong123456789@users.noreply.github.com>
2022-12-26 23:35:02 +0800

    Update Mushini.js

--- Mushini.js
+++ Mushini.js
@@ -1,13 +1,28 @@
 # Mushini
 私人订阅
 /*******************************
-⚠️🛝此捷径仅适用于本地重写⚠️🛝
-  🧚🏻‍♂️作者：🍡魔法师🍡
-  🎈交流群：993539991
-🫧脚本名称:
-🫧建议配合working copy一起食用
-✈️下载地址https://apps.apple.com/app/id896694807✈️
-*******************************/
-var body=$response.body;
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+巧影破解订阅
+脚本名称:巧影破解订阅
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^http[s]?:\/\/api-account.kinemasters.com\/v2\/user\/subscribe.+$ url script-response-body kinemasters.js
+[mitm] 
+hostname = *.kinemasters.*
+*******************************
+Surge
 
-$done(body);
+[Script]
+^http[s]?:\/\/api-account.kinemasters.com\/v2\/user\/subscribe.+$ requires-body=1,max-size=0,script-path=kinemasters.js
+
+[MITM]
+hostname = *.kinemasters.*
+
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.data.isSubscribed= true;
+    $done({body: JSON.stringify(obj)});
