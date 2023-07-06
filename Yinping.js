ff60ee1560ebe21cb2b89414f7a7f2a6318b7610
Wenlong123456789 <92168128+Wenlong123456789@users.noreply.github.com>
2023-07-06 18:16:40 +0800

    Update Yinping.js

--- Yinping.js
+++ Yinping.js
@@ -1,54 +1,23 @@
-{
-  "rules" : [
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "\"timeExpire\":\"2289-07-06 17:30:02\"",
-      "matchValue" : "\"timeExpire\":\"\"",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "\"timeStart\":\"2023-07-06 17:30:02\"",
-      "matchValue" : "\"timeStart\":\"\"",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "\"isR\":true",
-      "matchValue" : "\"isR\":\\w+",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "\"isSubscribe\":true",
-      "matchValue" : "\"isSubscribe\":\\w+",
-      "destiontion" : "response",
-      "isRegex" : true
-    }
-  ],
-  "enabled" : true,
-  "name" : "音频剪辑破解",
-  "description" : "",
-  "locations" : [
-    {
-      "method" : "",
-      "scheme" : "",
-      "enabled" : true,
-      "port" : 0,
-      "query" : "",
-      "host" : "pay.camoryapps.com",
-      "path" : "\/appPay\/api\/user\/info\/tokenLogin"
-    }
-  ]
-}
+/******************************
+⚠️如果放远程，请把Yinping.js替换成运程链接⚠️
+  🧚🏻‍♂️作者：🍡魔法师、木木🍡
+  wx交流群：1077223830
+🫧脚本名称:音频剪辑破解
+🫧建议配合working copy一起食用
+✈️working copy下载地址https://apps.apple.com/app/id896694807✈️
+*******************************
+%¥
+[rewrite_local]
+^https?:\/\/pay\.camoryapps\.com\/appPay\/api\/user\/info\/tokenLogin.*? url script-response-body Yinping.js
+
+
+[mitm]
+hostname = pay.camoryapps.com
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/"timeExpire\":""/g,'"timeExpire":"2289-07-06 17:30:02"');
+body = body.replace(/"timeStart\":""/g,'"timeStart":"2023-07-06 17:30:02"');
+body = body.replace(/"isR\":\w+/g,'"isR":true');
+body = body.replace(/"isSubscribe\":\w+/g,'"isSubscribe":true');
+$done(body);
