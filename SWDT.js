88e1ae89e5d5d1523b153d7ba2b9dc977739389d
Wenlong123456789 <92168128+Wenlong123456789@users.noreply.github.com>
2023-11-21 02:07:07 +0800

    Update SWDT.js

--- SWDT.js
+++ SWDT.js
@@ -8,7 +8,7 @@
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/mindline\.cn\/userSync.*? url script-response-body swdt.js
+^https?:\/\/mindline\.cn\/userSync.*? url script-response-body https://raw.githubusercontent.com/Wenlong123456789/Mushini/main/SWDT.js
 
 
 [mitm]
