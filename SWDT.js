24f07057e3ebbbd93ca354da11f882db520f7d29
Wenlong123456789 <92168128+Wenlong123456789@users.noreply.github.com>
2023-11-21 02:06:23 +0800

    Create SWDT.js

---
+++ SWDT.js
@@ -0,0 +1,22 @@
+/******************************
+⚠️如果放远程，请把swdt.js替换成运程链接⚠️
+  🧚🏻‍♂️作者：🍡魔法师、木木🍡
+  wx交流群：1077223830
+🫧脚本名称:思维导图
+🫧建议配合working copy一起食用
+✈️working copy下载地址https://apps.apple.com/app/id896694807✈️
+*******************************
+%¥
+[rewrite_local]
+^https?:\/\/mindline\.cn\/userSync.*? url script-response-body swdt.js
+
+
+[mitm]
+hostname = mindline.cn
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/"vipPeriod\":\d+/g,'"vipPeriod":1000');
+body = body.replace(/"paid\":\w+/g,'"paid":true');
+body = body.replace(/"purchasedDate\":\d+/g,'"purchasedDate":10082679308');
+$done(body);
