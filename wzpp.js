e928117e500e68136d2a267e77168a1c7b54353c
Wenlong123456789 <92168128+Wenlong123456789@users.noreply.github.com>
2024-04-20 17:52:12 +0800

    Create wzpp.js

---
+++ wzpp.js
@@ -0,0 +1,24 @@
+/******************************
+⚠️如果放远程，请把Wbpp.js替换成运程链接⚠️
+  🧚🏻‍♂️作者：🍡魔法师、木木🍡
+  wx交流群：1077223830
+🫧脚本名称:问真排盘
+🫧建议配合working copy一起食用
+✈️working copy下载地址https://apps.apple.com/app/id896694807✈️
+*******************************
+%¥
+[rewrite_local]
+^https?:\/\/bzpp2\.iwzbz\.com.*?.*? url script-response-body Wbpp.js
+
+
+[mitm]
+hostname = bzpp2.iwzbz.com
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/"vipLevel\": \d/g,'"vipLevel": 9');
+body = body.replace(/"expires\": ".*?"/g,'"expires": "2289-01-01 00:00:00"');
+body = body.replace(/"isSuccess\": \w+/g,'"isSuccess": true');
+body = body.replace(/"code\": -1/g,'"code": 0');
+body = body.replace(/"message\": ""/g,'"message": "你无敌啦"');
+$done(body);
