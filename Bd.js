05bb5c1316b99c1e4939704dc0cccccbad65647c
Wenlong123456789 <92168128+Wenlong123456789@users.noreply.github.com>
2025-05-26 17:27:28 +0800

    Update Bd.js

--- Bd.js
+++ Bd.js
@@ -5,9 +5,9 @@
 
 [rewrite_local]
 # 用户信息及VIP状态
-^https?:\/\/bd-api\.kuwo\.cn\/api\/ucenter\/users\/(pub\/35772808|login) url script-response-body https://raw.githubusercontent.com/yourname/repo/main/bd_music.js
+^https?:\/\/bd-api\.kuwo\.cn\/api\/ucenter\/users\/(pub\/35772808|login) url script-response-body https://raw.githubusercontent.com/Wenlong123456789/Mushini/refs/heads/main/Bd.js
 # 广告配置
-^https?:\/\/bd-api\.kuwo\.cn\/api\/service\/(advert\/config|global\/config\/vipEnter) url script-response-body https://raw.githubusercontent.com/yourname/repo/main/bd_music.js
+^https?:\/\/bd-api\.kuwo\.cn\/api\/service\/(advert\/config|global\/config\/vipEnter) url script-response-body https://raw.githubusercontent.com/Wenlong123456789/Mushini/refs/heads/main/Bd.js
 
 [mitm]
 hostname = bd-api.kuwo.cn
